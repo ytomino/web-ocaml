22f69c3215521e19090f20a189c162546830f25e
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-04 08:18:45 +0900

    Append unit parameters to partially apply
    This is a breaking change.

--- source/web.mli
+++ source/web.mli
@@ -19,10 +19,10 @@ val decode_content_type: string -> post_encoded
 val decode_multipart_form_data: string -> string StringMap.t
 
 val header_see_other: (string -> unit) -> string -> unit
-val header_service_unavailable: (string -> unit) -> unit
+val header_service_unavailable: (string -> unit) -> unit -> unit
 val header_content_type: (string -> unit) -> string -> unit
 val header_cookie: (string -> unit) -> ?expires:float -> string StringMap.t -> unit
-val header_break: (string -> unit) -> unit
+val header_break: (string -> unit) -> unit -> unit
 
 module CGI = Web__CGI
 module HTML = Web__HTML
