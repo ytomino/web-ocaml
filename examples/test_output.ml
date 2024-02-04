ece838da4df20240219332e004d680d8f748cf96
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-04 10:10:25 +0900

    Add Web.HTML.open_text and close_text
    And pack the parameters of Web.HTML.text_output_string.
    
    This is a breaking change.

--- examples/test_output.ml
+++ examples/test_output.ml
@@ -8,4 +8,17 @@ assert (
 	Buffer.contents b = "set-cookie: K=V; expires=Thu, 01 Jan 1970 00:00:00 GMT;\n"
 );;
 
+(* HTML text *)
+
+Buffer.clear b;;
+
+let out version = (
+	let tc = Web.HTML.open_text version (Buffer.add_string b) in
+	Web.HTML.text_output_string tc "\n";
+	Web.HTML.close_text tc
+) in
+out `html4;
+out `xhtml1;
+assert (Buffer.contents b = "<br><br />");;
+
 prerr_endline "ok";;
--- source/web__HTML.mli
+++ source/web__HTML.mli
@@ -2,5 +2,10 @@ type version = [`html4 | `html5 | `xhtml1 | `xhtml5 | `xml]
 
 val bool_of_checkbox: string -> bool
 
-val text_output_string: version -> (string -> unit) -> string -> unit
+type text_context
+
+val open_text: version -> (string -> unit) -> text_context
+val close_text: text_context -> unit
+val text_output_string: text_context -> string -> unit
+
 val attribute_output_string: version -> (string -> unit) -> string -> unit
