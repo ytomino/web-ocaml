2feeff033d19d0ddf529f536319ee0b6b216c335
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-05 09:39:24 +0900

    Add Web.CGI.remote_addr and remote_host

--- source/web__CGI.mli
+++ source/web__CGI.mli
@@ -6,3 +6,6 @@ val post: unit -> bool
 val post_encoded: unit -> post_encoded
 val post_length: unit -> int
 val read_input: unit -> string StringMap.t
+
+val remote_addr: unit -> string
+val remote_host: unit -> string
