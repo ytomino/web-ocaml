b1a2db58ab1804f6724046eeb15b70cc1605f650
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-15 21:37:46 +0900

    Do not ignore "\r" for writing text and attribute

--- source/web__HTML.ml
+++ source/web__HTML.ml
@@ -27,7 +27,8 @@ let br (version: version) = (
 module Text = struct
 	type text_context = {
 		version: version;
-		print_substring: string -> int -> int -> unit
+		print_substring: string -> int -> int -> unit;
+		mutable cr: bool
 	};;
 end;;
 
@@ -36,16 +37,25 @@ type text_context = Text.text_context;;
 let open_text (version: version)
 	(print_substring: string -> int -> int -> unit) =
 (
-	{Text.version; print_substring}
+	{Text.version; print_substring; cr = false}
 );;
 
-let close_text (_: text_context) = ();;
+let close_text (context: text_context) = (
+	let {Text.version; print_substring; cr} = context in
+	let print_string = make_print_string print_substring in
+	if cr then (
+		print_string (br version);
+		context.Text.cr <- false
+	)
+);;
 
 let unsafe_text_output_substring: text_context -> string -> int -> int ->
 	unit =
 	let rec loop context s start i end_pos = (
-		let {Text.version; print_substring} = context in
+		let {Text.version; print_substring; cr} = context in
+		assert (start = i || not cr);
 		let print_range = make_print_range print_substring in
+		let print_string = make_print_string print_substring in
 		if i >= end_pos then print_range s start i else
 		match s.[i] with
 		| '&' ->
@@ -57,18 +67,27 @@ let unsafe_text_output_substring: text_context -> string -> int -> int ->
 		| ' ' ->
 			substitute context s start i end_pos "&#32;"
 		| '\n' ->
-			substitute context s start i end_pos (br version)
-		| '\r' ->
+			context.Text.cr <- false;
 			print_range s start i;
+			print_string (br version);
+			let next = i + 1 in
+			loop context s next next end_pos
+		| '\r' ->
+			if not cr then (
+				print_range s start i;
+				context.Text.cr <- true
+			) else print_string (br version);
 			let next = i + 1 in
 			loop context s next next end_pos
 		| _ ->
+			close_text context;
 			loop context s start (i + 1) end_pos
 	) and substitute context s start i end_pos cer = (
 		let {Text.print_substring; _} = context in
 		let print_range = make_print_range print_substring in
 		let print_string = make_print_string print_substring in
-		print_range s start i;
+		if start < i then print_range s start i
+		else close_text context;
 		print_string cer;
 		let next = i + 1 in
 		loop context s next next end_pos
@@ -94,6 +113,12 @@ let newline (version: version) = (
 	| `xhtml1 | `xhtml5 | `xml -> "&#10;"
 );;
 
+let string_of_cr (version: version) = (
+	match version with
+	| `html4 -> "\n" (* for very old browser *)
+	| `html5 | `xhtml1 | `xhtml5 | `xml -> "&#13;"
+);;
+
 let apos (version: version) = (
 	match version with
 	| `xhtml1 -> "&#39;"
@@ -104,11 +129,21 @@ let apos (version: version) = (
 module Attribute = struct
 	type attribute_context = {
 		version: version;
-		print_substring: string -> int -> int -> unit
+		print_substring: string -> int -> int -> unit;
+		mutable cr: bool
 	};;
 end;;
 
 type attribute_context = Attribute.attribute_context;;
+
+let flush_cr (context: attribute_context) = (
+	let {Attribute.version; print_substring; cr} = context in
+	let print_string = make_print_string print_substring in
+	if cr then (
+		print_string (string_of_cr version);
+		context.Attribute.cr <- false
+	)
+);;
 
 let open_attribute (version: version)
 	(print_substring: string -> int -> int -> unit) (name: string) =
@@ -117,10 +152,11 @@ let open_attribute (version: version)
 	print_string " ";
 	print_string name;
 	print_string "=\"";
-	{Attribute.version; print_substring}
+	{Attribute.version; print_substring; cr = false}
 );;
 
 let close_attribute (context: attribute_context) = (
+	flush_cr context;
 	let {Attribute.print_substring; _} = context in
 	let print_string = make_print_string print_substring in
 	print_string "\""
@@ -129,8 +165,10 @@ let close_attribute (context: attribute_context) = (
 let unsafe_attribute_output_substring: attribute_context -> string -> int ->
 	int -> unit =
 	let rec loop context s start i end_pos = (
-		let {Attribute.version; print_substring} = context in
+		let {Attribute.version; print_substring; cr} = context in
+		assert (start = i || not cr);
 		let print_range = make_print_range print_substring in
+		let print_string = make_print_string print_substring in
 		if i >= end_pos then print_range s start i else
 		match s.[i] with
 		| '&' ->
@@ -146,18 +184,34 @@ let unsafe_attribute_output_substring: attribute_context -> string -> int ->
 		| '\"' ->
 			substitute context s start i end_pos "&quot;"
 		| '\n' ->
-			substitute context s start i end_pos (newline version)
+			if start < i then print_range s start i else
+			if cr then (
+				(* substitute "\r" or "\r\n" to "\n" for HTML4 *)
+				begin match version with
+				| `html4 -> ()
+				| `html5 | `xhtml1 | `xhtml5 | `xml -> print_string (string_of_cr version)
+				end;
+				context.Attribute.cr <- false
+			);
+			print_string (newline version);
+			let next = i + 1 in
+			loop context s next next end_pos
 		| '\r' ->
-			print_range s start i;
+			if not cr then (
+				print_range s start i;
+				context.Attribute.cr <- true
+			) else print_string (string_of_cr version);
 			let next = i + 1 in
 			loop context s next next end_pos
 		| _ ->
+			flush_cr context;
 			loop context s start (i + 1) end_pos
 	) and substitute context s start i end_pos cer = (
 		let {Attribute.print_substring; _} = context in
 		let print_range = make_print_range print_substring in
 		let print_string = make_print_string print_substring in
-		print_range s start i;
+		if start < i then print_range s start i
+		else flush_cr context;
 		print_string cer;
 		let next = i + 1 in
 		loop context s next next end_pos
