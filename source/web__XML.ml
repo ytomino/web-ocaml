6eba0f84ce4836040d1aac8cdb4bbd0494276074
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-29 02:25:25 +0900

    Add Web.XML.open_text, close_text, and outputting functions
    text_output_substring and text_output_string.

--- source/web__XML.ml
+++ source/web__XML.ml
@@ -19,3 +19,16 @@ let xml_declaration (print_substring: string -> int -> int -> unit)
 	end;
 	print_substring "?>\n" 0 (if endline then 3 else 2)
 );;
+
+type text_context = Web__HTML.text_context;;
+
+let open_text: (string -> int -> int -> unit) -> text_context =
+	Web__HTML.open_text `xml ?space:None ?newline:None;;
+
+let close_text: text_context -> unit = Web__HTML.close_text;;
+
+let text_output_substring: text_context -> string -> int -> int -> unit =
+	Web__HTML.text_output_substring;;
+
+let text_output_string: text_context -> string -> unit =
+	Web__HTML.text_output_string;;
