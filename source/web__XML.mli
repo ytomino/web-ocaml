d7b2cea7e078934110979a497249ad8c244da0f7
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-29 03:43:58 +0900

    Add Web.XML.open_attribute, close_attribute, and outputting functions
    attribute_output_substring and attribute_output_string.

--- source/web__XML.mli
+++ source/web__XML.mli
@@ -7,3 +7,12 @@ val open_text: (string -> int -> int -> unit) -> text_context
 val close_text: text_context -> unit
 val text_output_substring: text_context -> string -> int -> int -> unit
 val text_output_string: text_context -> string -> unit
+
+type attribute_context
+
+val open_attribute: (string -> int -> int -> unit) -> string ->
+	attribute_context
+val close_attribute: attribute_context -> unit
+val attribute_output_substring: attribute_context -> string -> int -> int ->
+	unit
+val attribute_output_string: attribute_context -> string -> unit
