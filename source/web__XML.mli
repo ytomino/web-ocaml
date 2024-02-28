32d0790e4c6b9049bfda4eee57ae97eb97289a8a
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-29 02:25:25 +0900

    Add Web.XML.open_text, close_text, and outputting functions
    text_output_substring and text_output_string.

--- source/web__XML.mli
+++ source/web__XML.mli
@@ -1,2 +1,9 @@
 val xml_declaration: (string -> int -> int -> unit) -> ?encoding:string ->
 	?endline:bool -> unit -> unit
+
+type text_context
+
+val open_text: (string -> int -> int -> unit) -> text_context
+val close_text: text_context -> unit
+val text_output_substring: text_context -> string -> int -> int -> unit
+val text_output_string: text_context -> string -> unit
